17b2e4a884e2d086d581bbeb7a5b153d498d9dc4
code naveen <iitiannaveen175@gmail.com>
2022-12-23 14:33:01 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.alarms.create({
+    periodInMinutes: 1/60,
+})
+chrome.alarms.onAlarm.addListener((alarm)=>{
+    chrome.storage.local.get(["timer"],(res)=>{
+        const time=res.timer ?? 0
+        chrome.storage.local.set({
+            timer: time+1,
+        })
+        chrome.action.setBadgeText({
+            text: `${time+1}`,
+        })
+        if(time%1000==0){
+        this.registration.showNotification("Chrome Timer Extention",{
+            body: "1 second has passed!",
+            icon: "icon.png",
+        })  
+        }
+    })
+})
---
+++ popup.js
@@ -0,0 +1,26 @@
+const timeElement=document.getElementById("time")
+const nameElement=document.getElementById("name")
+const  timerElement= document.getElementById("timer")
+
+function updateTimeElements(){
+    chrome.storage.local.get(["timer"],(res)=>{
+        const time=res.timer??0
+        timerElement.textContent=`The timer is at: ${time} seconds`
+    })
+    const currentTime= new Date().toLocaleTimeString()
+
+    timeElement.textContent=`The time is: ${currentTime}`
+
+}
+
+updateTimeElements()
+setInterval(updateTimeElements,1000);
+
+
+
+
+
+
+chrome.storage.sync.get(["name"],(res)=>{
+    nameElement.textContent=`Your name id: ${res.name}`
+})
